55c686e57b4f594572f9587c2dda6ce649f6854b
Andres Fornaris - OLE <andres.fornaris@olelife.com>
2025-07-25 15:41:50 -0500

    Update slackNotify.groovy

--- vars/slackNotify.groovy
+++ vars/slackNotify.groovy
@@ -1,8 +1,53 @@
-
 def call(Map config = [:]) {
-    def channel = config.channel ?: '#tech-deploys'
-    def color = config.color ?: 'good'
-    def message = config.message ?: "Mensaje por defecto de notificación."
+    def channel      = config.channel ?: '#tech-deploys'
+    def color        = config.color ?: 'good'
+    def includeLog   = config.includeLog ?: false
+    def showStatus   = config.get('showStatus', true)  // Nuevo flag
+    def buildUrl     = env.BUILD_URL ?: ''
+    def jobName      = env.JOB_NAME ?: ''
+    def buildNumber  = env.BUILD_NUMBER ?: ''
+    def result       = currentBuild.currentResult ?: 'UNKNOWN'
+    def triggeredBy  = "Sistema"
+    def emoji        = ":robot_face:"
+
+    // Determinar quién lo ejecutó
+    try {
+        def userCause = currentBuild.rawBuild.getCauses().find { it instanceof hudson.model.Cause$UserIdCause }
+        if (userCause) {
+            triggeredBy = userCause.userName
+            emoji = triggeredBy.toLowerCase() in ['admin', 'andres fornaris'] ? ":crown:" : ":bust_in_silhouette:"
+        } else {
+            def gitAuthor = sh(script: "git log -1 --pretty=format:'%an'", returnStdout: true).trim()
+            if (gitAuthor) {
+                triggeredBy = "Git Push por ${gitAuthor}"
+                emoji = ":octocat:"
+            }
+        }
+    } catch (e) {
+        triggeredBy = "Desconocido"
+    }
+
+    // Armar mensaje
+    def message = "*${emoji} ${jobName}* #${buildNumber}"
+
+    if (showStatus) {
+        message += " terminó con estado: *${result}*"
+    } else {
+        message += " ha iniciado"
+    }
+
+    if (includeLog && result == 'FAILURE') {
+        try {
+            def rawLog = currentBuild.rawBuild.getLog(100)
+            def errorLines = rawLog.findAll { it =~ /(?i)(error|exception|fail)/ }
+            def logSnippet = errorLines ? errorLines.join('\n') : rawLog.takeRight(20).join('\n')
+            message += "\n```" + logSnippet.take(1000) + "```"
+        } catch (e) {
+            message += "\n_(No se pudo extraer el log de error)_"
+        }
+    }
+
+    message += "\n👤 Desplegado por: *${triggeredBy}* (<${buildUrl}|Ver ejecución>)"
 
     slackSend(
         channel: channel,
