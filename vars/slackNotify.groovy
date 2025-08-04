252c29d7f2b4416b4c1efb41c7047fbbd299abbe
Andres Fornaris - OLE <andres.fornaris@olelife.com>
2025-08-04 11:36:21 -0500

    Update slackNotify.groovy]

--- vars/slackNotify.groovy
+++ vars/slackNotify.groovy
@@ -2,15 +2,14 @@ def call(Map config = [:]) {
     def channel      = config.channel ?: '#tech-deploys'
     def color        = config.color ?: 'good'
     def includeLog   = config.includeLog ?: false
-    def showStatus   = config.get('showStatus', true)  // Nuevo flag
+    def showStatus   = config.get('showStatus', true)
     def buildUrl     = env.BUILD_URL ?: ''
     def jobName      = env.JOB_NAME ?: ''
     def buildNumber  = env.BUILD_NUMBER ?: ''
-    def result       = currentBuild.currentResult ?: 'UNKNOWN'
     def triggeredBy  = "Sistema"
     def emoji        = ":robot_face:"
 
-    // Determinar quién lo ejecutó
+    // Determinar quién ejecutó el build
     try {
         def userCause = currentBuild.rawBuild.getCauses().find { it instanceof hudson.model.Cause$UserIdCause }
         if (userCause) {
@@ -27,16 +26,25 @@ def call(Map config = [:]) {
         triggeredBy = "Desconocido"
     }
 
-    // Armar mensaje
-    def message = "*${emoji} ${jobName}* #${buildNumber}"
-
+    // Estado y duración solo si es post (showStatus = true)
+    def result = "EN PROGRESO"
+    def durationStr = ""
     if (showStatus) {
-        message += " terminó con estado: *${result}*"
-    } else {
-        message += " ha iniciado"
+        result = currentBuild.currentResult ?: 'UNKNOWN'
+        if (result in ['SUCCESS', 'FAILURE', 'UNSTABLE']) {
+            def durationMs = currentBuild.duration ?: 0
+            def minutes = (durationMs / 1000 / 60).intValue()
+            def seconds = ((durationMs / 1000) % 60).intValue()
+            durationStr = "\n⏱️ *Duración:* ${minutes}m ${seconds}s"
+        }
     }
 
-    if (includeLog && result == 'FAILURE') {
+    // Construir mensaje
+    def message = "*${emoji} ${jobName}* #${buildNumber}"
+    message += showStatus ? " terminó con estado: *${result}*" : " ha iniciado"
+
+    // Logs de error si falla
+    if (includeLog && showStatus && result == 'FAILURE') {
         try {
             def rawLog = currentBuild.rawBuild.getLog(100)
             def errorLines = rawLog.findAll { it =~ /(?i)(error|exception|fail)/ }
@@ -48,6 +56,7 @@ def call(Map config = [:]) {
     }
 
     message += "\n👤 Desplegado por: *${triggeredBy}* (<${buildUrl}|Ver ejecución>)"
+    message += durationStr
 
     slackSend(
         channel: channel,
